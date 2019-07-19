fbf1624ac2347a727d353ecd4ef8054298f529b9
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-19 21:39:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+function happyHolidaysTo(pers) {
+  return 'Happy holidays, ${pers}!'
 }
 
 function happyHolidayTo(holiday, name) {
